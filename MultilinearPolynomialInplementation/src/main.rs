03292c02d572e5d82d0940cc2df699ec6d0a5433
sogobanwo <sogobanwo@gmail,com>
2025-01-24 14:18:41 +0100

    feat: added the evaluation of multi-linear polynomials

--- MultilinearPolynomialInplementation/src/main.rs
+++ MultilinearPolynomialInplementation/src/main.rs
@@ -1,22 +1,19 @@
 use ark_ff::PrimeField;
 use ark_bn254::Fr;
 
-
-struct Multilinear<T: PrimeField> {
-    polynomial: Vec<T>,
-}
-impl<T: PrimeField > Multilinear <T> {
-    fn partial_evaluation(&self, evaluation_points: Vec<T>, evaluation_variable_index: usize) -> Vec<T> {
+fn partial_evaluation<T: PrimeField>(polynomial: Vec<T>, evaluation_points: Vec<T>, evaluation_variable_index: usize) -> Vec<T> {
         let mut result: Vec<T> = Vec::new();
         let mut i = 0;
         let mut j = 0;
         let binary: usize = 2;
+
         let jump:usize = binary.pow((evaluation_points.len() - evaluation_variable_index - 1).try_into().unwrap());
+        
         let langrange_basis_zero = T::one() - evaluation_points[evaluation_variable_index];
         let langrange_basis_one = evaluation_points[evaluation_variable_index];
 
-        while i < self.polynomial.len() && j < evaluation_points.len() {
-            result.push(self.polynomial[i] * (langrange_basis_zero) + self.polynomial[i + jump] * (langrange_basis_one));
+        while i < polynomial.len() && j < evaluation_points.len() {
+            result.push(polynomial[i] * (langrange_basis_zero) + polynomial[i + jump] * (langrange_basis_one));
             if (i + 1) % jump != 0 {
                 i = i + 1
             } else {
@@ -26,29 +23,45 @@ impl<T: PrimeField > Multilinear <T> {
         }
         result
     }
+
+fn evaluate<T: PrimeField>(mut polynomial: Vec<T>, mut evaluation_points: Vec<T>) -> T {
+  let times_to_evaluate = evaluation_points.len();
+  polynomial = partial_evaluation(polynomial, evaluation_points.clone(), 0);
+
+  let mut i = 1;
+
+  while i < times_to_evaluate {
+    evaluation_points.remove(0);
+    polynomial = partial_evaluation(polynomial.clone(), evaluation_points.clone(), 0);
+
+    println!("Got Here");
+    println!("{:?}", polynomial);
+    i +=1 ;
+  }
+
+  polynomial[0]
 }
 
+
 fn main() {
-    let mlp = Multilinear {
-        polynomial: vec![Fr::from(0), Fr::from(2), Fr::from(0), Fr::from(5)]
-    };
-
-    let res = mlp.partial_evaluation(vec![Fr::from(5), Fr::from(0)], 0);
-
+    let res = partial_evaluation(vec![Fr::from(0), Fr::from(2), Fr::from(0), Fr::from(5)], vec![Fr::from(5), Fr::from(0)], 0);
     println!("{:?}", res);
 }
 
 #[cfg(test)]
 mod tests{
-    use crate::Multilinear;
+    use super::*;
     use ark_bn254::Fq;
     #[test]
     fn partial_evaluation_test(){
-        let p = Multilinear {
-            polynomial: vec![Fq::from(0), Fq::from(2), Fq::from(0), Fq::from(5)],
-        };
-        let partial_evaluated_result = p.partial_evaluation(vec![Fq::from(5), Fq::from(0)], 0);
+        let partial_evaluated_result = partial_evaluation(vec![Fq::from(0), Fq::from(2), Fq::from(0), Fq::from(5)], vec![Fq::from(5), Fq::from(0)], 0);
         assert_eq!(partial_evaluated_result, vec![Fq::from(0), Fq::from(17)]);
+    }
+
+    #[test]
+    fn evaluate_test(){
+        let partial_evaluated_result = evaluate(vec![Fq::from(0), Fq::from(0), Fq::from(3), Fq::from(8)], vec![Fq::from(6), Fq::from(2)]);
+        assert_eq!(partial_evaluated_result, Fq::from(78));
     }
 }
 
