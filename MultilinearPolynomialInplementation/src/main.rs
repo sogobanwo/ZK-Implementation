1c6d63fdd65799ca1ebf2a346e6a3560c739fbb1
sogobanwo <sogobanwo@gmail,com>
2025-01-26 21:30:13 +0100

    feat: added the evaluation of multi-linear polynomials

--- MultilinearPolynomialInplementation/src/main.rs
+++ MultilinearPolynomialInplementation/src/main.rs
@@ -2,27 +2,31 @@ use ark_ff::PrimeField;
 use ark_bn254::Fr;
 
 fn partial_evaluation<T: PrimeField>(polynomial: Vec<T>, evaluation_points: Vec<T>, evaluation_variable_index: usize) -> Vec<T> {
-        let mut result: Vec<T> = Vec::new();
-        let mut i = 0;
-        let mut j = 0;
-        let binary: usize = 2;
+    let mut result: Vec<T> = Vec::new();
+    let mut i = 0;
+    let mut j = 0;
+    let binary: usize = 2;
+    let expected_poly_size = polynomial.len()/2;
 
-        let jump:usize = binary.pow((evaluation_points.len() - evaluation_variable_index - 1).try_into().unwrap());
-        
-        let langrange_basis_zero = T::one() - evaluation_points[evaluation_variable_index];
-        let langrange_basis_one = evaluation_points[evaluation_variable_index];
+    let jump:usize = binary.pow((evaluation_points.len() - evaluation_variable_index - 1).try_into().unwrap());
 
-        while i < polynomial.len() && j < evaluation_points.len() {
-            result.push(polynomial[i] * (langrange_basis_zero) + polynomial[i + jump] * (langrange_basis_one));
-            if (i + 1) % jump != 0 {
-                i = i + 1
-            } else {
-                i = i + jump;
-            }
-            j+=1
+    let langrange_basis_zero = T::one() - evaluation_points[evaluation_variable_index];
+    let langrange_basis_one = evaluation_points[evaluation_variable_index];
+
+    while i < expected_poly_size {
+        result.push(polynomial[j] * (langrange_basis_zero) + polynomial[j + jump] * (langrange_basis_one));
+
+        if (j + 1) % jump != 0 {
+            j = j + 1
+        } else {
+            j = j + jump + 1;
         }
-        result
+
+        i += 1;
     }
+    result
+}
+
 
 fn evaluate<T: PrimeField>(mut polynomial: Vec<T>, mut evaluation_points: Vec<T>) -> T {
   let times_to_evaluate = evaluation_points.len();
@@ -54,8 +58,8 @@ mod tests{
     use ark_bn254::Fq;
     #[test]
     fn partial_evaluation_test(){
-        let partial_evaluated_result = partial_evaluation(vec![Fq::from(0), Fq::from(2), Fq::from(0), Fq::from(5)], vec![Fq::from(5), Fq::from(0)], 0);
-        assert_eq!(partial_evaluated_result, vec![Fq::from(0), Fq::from(17)]);
+        let partial_evaluated_result = partial_evaluation(vec![Fq::from(0), Fq::from(0), Fq::from(0), Fq::from(3),Fq::from(0),Fq::from(0), Fq::from(2),Fq::from(5)], vec![Fq::from(5), Fq::from(0), Fq::from(3)], 2);
+        assert_eq!(partial_evaluated_result, vec![Fq::from(0), Fq::from(9), Fq::from(0), Fq::from(11)]);
     }
 
     #[test]
